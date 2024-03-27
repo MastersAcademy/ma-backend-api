232287e6ee82d2802d1c87069e2582fa49dcc4e3
MZabolotnev <mzabolotnev@liveperson.com>
2024-03-27 13:09:34 +0200

    change db to postgres, add Dockerfile

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -6,16 +6,17 @@ import { ENTITIES } from '@db/entities';
 @Global()
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'better-sqlite3',
-        database: ':memory:',
-        synchronize: true,
-        logging: false,
-        entities: [...ENTITIES],
-      }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [...ENTITIES],
+      synchronize: true,
     }),
-    TypeOrmModule.forFeature(ENTITIES),
+    TypeOrmModule.forFeature([...ENTITIES]),
   ],
   exports: [TypeOrmModule],
 })
